b8a28f753d14add88233cf4eddfa35e38c01be15
Guilherme Amorim <98614355+GuiAmorim03@users.noreply.github.com>
2024-10-10 22:53:52 +0100

    remove non-necessary css

--- supplywise-ui/src/components/navbar.jsx
+++ supplywise-ui/src/components/navbar.jsx
@@ -7,25 +7,6 @@ import Link from 'next/link';
 
 
 export default function Navbar() {
-    useEffect(() => {
-
-        var navbarShrink = function () {
-            const navbarCollapsible = document.body.querySelector('#mainNav');
-            if (!navbarCollapsible) {
-                return;
-            }
-            navbarCollapsible.classList.add('navbar-shrink')
-
-        };
-
-        navbarShrink();
-        document.addEventListener('scroll', navbarShrink);
-    });
-
-    useEffect(() => {
-        // import bootstrap after the component is mounted
-        import('bootstrap/dist/js/bootstrap.bundle.min.js')
-    }, []);
 
     const router = useRouter();
     const [currentPage, setCurrentPage] = useState('');
@@ -42,18 +23,18 @@ export default function Navbar() {
                 <Link className="navbar-brand text-black" href="/">
                     SupplyWise
                 </Link>
-                <button className="navbar-toggler text-uppercase bg-secondary rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={faBars} />
+                <button className="navbar-toggler text-uppercase sw-bgcolor text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faSearch} />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ms-auto fs-5">
                         <li className="nav-item mx-0 mx-lg-1">
                             <Link className={`nav-link py-3 px-0 px-lg-3 rounded text-black`} href="/about">
-                                Documetation
+                                Documentation
                             </Link>
                         </li>
                         <li className="nav-item mx-0 mx-lg-1">
-                            <Link className={`nav-link py-3 px-0 px-lg-3 rounded-pill sw-bgcolor fs-4`} href="/login">
+                            <Link className={`nav-link py-3 px-0 px-lg-3 rounded-pill sw-bgcolor text-white fs-4`} href="/login">
                                 Login
                             </Link>
                         </li>
