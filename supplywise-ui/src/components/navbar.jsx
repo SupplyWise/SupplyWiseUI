f72fe5f84c2e863afea7fa9d46f09faf0486a9f6
Alex <71047866+Sytuz@users.noreply.github.com>
2024-10-10 14:20:26 +0100

    feat: landing page

--- supplywise-ui/src/components/navbar.jsx
+++ supplywise-ui/src/components/navbar.jsx
@@ -37,29 +37,24 @@ export default function Navbar() {
 
 
     return (
-        <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div className="container">
-                <Link className="navbar-brand" href="/">
+        <nav className="navbar navbar-expand-lg bg-light fixed-top shadow" id="mainNav">
+            <div className="container-fluid" style={{width: '90%'}}>
+                <Link className="navbar-brand text-black" href="/">
                     SupplyWise
                 </Link>
-                <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler text-uppercase bg-secondary rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto">
+                    <ul className="navbar-nav ms-auto fs-5">
                         <li className="nav-item mx-0 mx-lg-1">
-                            <Link className={`nav-link py-3 px-0 px-lg-3 rounded ${currentPage === '/' ? 'active' : ''}`} href="/">
-                                Home
+                            <Link className={`nav-link py-3 px-0 px-lg-3 rounded text-black`} href="/about">
+                                Documetation
                             </Link>
                         </li>
                         <li className="nav-item mx-0 mx-lg-1">
-                            <Link className={`nav-link py-3 px-0 px-lg-3 rounded ${currentPage === '/diseases' ? 'active' : ''}`} href="/pricing">
-                                Pricing
-                            </Link>
-                        </li>
-                        <li className="nav-item mx-0 mx-lg-1">
-                            <Link className={`nav-link py-3 px-0 px-lg-3 rounded ${currentPage === '/about' ? 'active' : ''}`} href="/about">
-                                About
+                            <Link className={`nav-link py-3 px-0 px-lg-3 rounded-pill sw-bgcolor fs-4`} href="/login">
+                                Login
                             </Link>
                         </li>
                     </ul>
