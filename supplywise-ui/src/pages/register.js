4df29bc59443ce5d96f32eb60e7df89e936af44f
Alex <71047866+Sytuz@users.noreply.github.com>
2024-10-22 21:59:55 +0100

    feat: login and register feature

--- supplywise-ui/src/pages/register.js
+++ supplywise-ui/src/pages/register.js
@@ -1,28 +1,73 @@
 import Head from 'next/head';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { API_URL } from '../../api_url';
 
 export default function Register() {
+    const router = useRouter();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
+    const [fullname, setFullname] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (password !== confirmPassword) {
-            alert('Passwords do not match');
+    const handleRegister = () => {
+        document.getElementById('register-error').classList.add('visually-hidden');
+        try {
+          if (password !== confirmPassword) {
+            const registerError = document.getElementById('register-error');
+            registerError.innerText = 'Passwords do not match';
+            registerError.classList.remove('visually-hidden');
             return;
+          }
+    
+          fetch(`${API_URL}/auth/register`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              "name": fullname,
+              "email": email,
+              "password": password,
+            }),
+          })
+            .then((response) => {
+              if (!response.ok) {
+                throw new Error(response.statusText || 'HTTP error, status = ' + response.status);
+              }
+              return response.text().then((text) => text ? JSON.parse(text) : {});
+            })
+            .then((data) => {
+              sessionStorage.setItem('sessionToken', data.token);
+              router.push('/dashboard');
+            })
+            .catch((error) => {
+              const registerError = document.getElementById('register-error');
+              console.error(error);
+              if (error.message.includes('400')) {
+                registerError.innerText = 'Invalid user';
+              } else if (error.message.includes('409')) {
+                registerError.innerText = 'User already registered';
+              } else {
+                registerError.innerText = 'An error occurred while registering the user';
+              }
+              registerError.classList.remove('visually-hidden');
+            });
+    
+        } catch (error) {
+          const registerError = document.getElementById('register-error');
+          console.error(error);
+          if (error.message.includes('400')) {
+            registerError.innerText = 'Invalid user';
+          } else if (error.message.includes('409')) {
+            registerError.innerText = 'User already registered';
+          } else {
+            registerError.innerText = 'An error occurred while registering the user';
+          }
+          registerError.classList.remove('visually-hidden');
         }
-
-        // Dummy validation for registration
-        if (username && email && password) {
-            sessionStorage.setItem('user', JSON.stringify({ email }));
-            window.location.href = '/dashboard';
-        } else {
-            alert('Please fill all the fields');
-        }
-    };
+      };    
 
     return (
         <main>
@@ -35,15 +80,15 @@ export default function Register() {
             <div className="container d-flex justify-content-center align-items-center vh-100">
                 <div className="card p-4 shadow-lg" style={{ width: '400px' }}>
                     <h2 className="text-center mb-4">Register</h2>
-                    <form onSubmit={handleSubmit}>
+                    <form>
                         <div className="form-group mb-3">
                             <input
-                                type="username"
+                                type="fullname"
                                 className="form-control"
-                                id="username"
-                                placeholder="Username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
+                                id="fullname"
+                                placeholder="Full Name"
+                                value={fullname}
+                                onChange={(e) => setFullname(e.target.value)}
                                 required
                             />
                         </div>
@@ -80,7 +125,11 @@ export default function Register() {
                                 required
                             />
                         </div>
-                        <button type="submit" className="btn text-white border-0 sw-bgcolor w-100">Register</button>
+                        <div id="register-error" className='visually-hidden text-danger'>
+                          &nbsp;
+                          <span>Invalid credentials</span>
+                        </div>
+                        <button type="button" onClick={handleRegister} className="btn text-white border-0 sw-bgcolor w-100">Register</button>
                     </form>
                     <div className="mt-3 text-center">
                         <span className="sw-color forgot-password" onClick={() => window.location.href = '/login'}>
