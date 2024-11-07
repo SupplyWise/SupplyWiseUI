3d483332024f0d6785f9a7af564cf00189faecc9
Guilherme Amorim <98614355+GuiAmorim03@users.noreply.github.com>
2024-11-07 23:33:19 +0000

    feat: reports page

--- supplywise-ui/src/pages/management/restaurants/reports.js
+++ supplywise-ui/src/pages/management/restaurants/reports.js
@@ -1,15 +1,164 @@
 
 import DashboardLayout from "@/components/managementLayout";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 export default function Restaurants() {
 
+    const reports = [
+        {
+            id: 1,
+            dateStart: "2024-01-01",
+            dateEnd: "2024-01-31",
+            numProducts: 10,
+            closedBy: "John Doe",
+        },
+        {
+            id: 2,
+            dateStart: "2024-02-01",
+            dateEnd: "2024-02-28",
+            numProducts: 10,
+            closedBy: "John Doe",
+        },
+        {
+            id: 3,
+            dateStart: "2024-03-01",
+            dateEnd: "2024-03-31",
+            numProducts: 10,
+            closedBy: "Mary Jane",
+        },
+        {
+            id: 4,
+            dateStart: "2024-04-01",
+            dateEnd: "2024-04-30",
+            numProducts: 10,
+            closedBy: "Mary Jane",
+        },
+        {
+            id: 5,
+            dateStart: "2024-05-01",
+            dateEnd: "2024-05-31",
+            numProducts: 10,
+            closedBy: "Mary Jane",
+        },
+        {
+            id: 6,
+            dateStart: "2024-06-01",
+            dateEnd: "2024-06-30",
+            numProducts: 10,
+            closedBy: "Mary Jane",
+        },
+        {
+            id: 7,
+            dateStart: "2024-07-01",
+            dateEnd: "2024-07-31",
+            numProducts: 10,
+            closedBy: "Mary Jane",
+        },
+        {
+            id: 8,
+            dateStart: "2024-08-01",
+            dateEnd: "2024-08-31",
+            numProducts: 10,
+            closedBy: "Mary Jane",
+        },
+        {
+            id: 9,
+            dateStart: "2024-09-01",
+            dateEnd: "2024-09-30",
+            numProducts: 10,
+            closedBy: "John Doe",
+        },
+        {
+            id: 10,
+            dateStart: "2024-10-01",
+            dateEnd: "2024-10-31",
+            numProducts: 10,
+            closedBy: "John Doe",
+        },
+        {
+            id: 11,
+            dateStart: "2024-11-01",
+            dateEnd: "2024-11-30",
+            numProducts: 10,
+            closedBy: "John Doe",
+        },
+        {
+            id: 12,
+            dateStart: "2024-12-01",
+            dateEnd: "2024-12-31",
+            numProducts: 10,
+            closedBy: "John Doe",
+        },
+    ]
+
+    var closers = []
+    reports.map((report) => {
+        if (!closers.includes(report.closedBy)) {
+            closers.push(report.closedBy)
+        }
+    })
+
+    const [dateStart, setDateStart] = useState('')
+    const [dateEnd, setDateEnd] = useState('')
+    const [closedBy, setClosedBy] = useState('')
+
+
     return (
         <DashboardLayout>
-            <div style={{ padding: '20px', marginTop: '80px' }}>
+            <div style={{ padding: '20px' }}>
                 <div className="content">
-                    <h1 className="title" style={{ fontSize: "36px", marginBottom: '10px', textAlign: 'center' }}>
-                        Welcome to reports!
-                    </h1>
+                    <div className='mt-3 row'>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row' }}>
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <label style={{width:'200%'}} className='form-label'>Filter by Date Interval</label>
+                                <input type="date" onChange={(e) => setDateStart(e.target.value)} className='form-control' />
+                                <input type="date" onChange={(e) => setDateEnd(e.target.value)} className='form-control' />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <label style={{width:'200%'}} className='form-label'>Closed By</label>
+                                <select className='form-select' onChange={(e) => setClosedBy(e.target.value)}>
+                                    <option selected value=''>All</option>
+                                    {closers.map((closer) => (
+                                        <option value={closer}>{closer}</option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+                        <table className="table table-striped mt-4" style={{ border: '2px solid black' }}>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Date Start</th>
+                                    <th scope="col">Date End</th>
+                                    <th scope="col">Number of Products</th>
+                                    <th scope="col">Closed By</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {reports
+                                    .filter(report =>
+                                        (dateStart === '' || report.dateStart >= dateStart) &&
+                                        (dateEnd === '' || report.dateEnd <= dateEnd) &&
+                                        (closedBy === '' || report.closedBy === closedBy)
+                                    )
+                                    .map((report) => (
+                                        <tr key={report.id}>
+                                            <td>{report.dateStart}</td>
+                                            <td>{report.dateEnd}</td>
+                                            <td>{report.numProducts}</td>
+                                            <td>{report.closedBy}</td>
+                                            <td>
+                                                <button className='btn btn-success ms-2'>
+                                                    <FontAwesomeIcon style={{ width: '.9vw' }} icon={faDownload} />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </DashboardLayout>
